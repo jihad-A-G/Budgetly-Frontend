ab81502d9cfd515c8be1b4c4a6031784aa8c24c7
anitta-sleiman <sleiman3198@gmail.com>
2023-11-28 09:16:49 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 // import Signup from './Auth/signup.jsx'
 import Login from './Auth/login.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Login/>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
